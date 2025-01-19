bbb9beef460cbb808fb6b978823c82e0add6b833
youngwoo <dudaks0805@gmail.com>
2025-01-20 07:10:37 +0900

    first project

---
+++ src/main/java/com/msacomponet/MsaComponetApplication.java
@@ -0,0 +1,13 @@
+package com.msacomponet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaComponetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaComponetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msacomponet/MsaComponetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msacomponet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaComponetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
