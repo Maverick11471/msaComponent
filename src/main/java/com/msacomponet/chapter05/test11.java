c87d9ecd43f6f72b879c1577543a030409525e19
youngwoo <dudaks0805@gmail.com>
2025-03-03 22:46:15 +0900

    feat: test11 추가
    수학 공부파일 1개 추가

---
+++ src/main/java/com/msacomponet/chapter05/test11.java
@@ -0,0 +1,15 @@
+package com.msacomponet.chapter05;
+
+import java.io.*;
+
+public class test11 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine(); // 입력값을 받아오지만 사용하지 않음
+        System.out.println(1); // 수행 횟수
+        System.out.println(0); // 시간 복잡도의 최고차항 차수
+    }
+
+
+}
